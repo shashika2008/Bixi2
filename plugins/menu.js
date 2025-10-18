88c3da399416cdc394548d49cfb02fb59f8c3a03
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-18 19:49:53 +0530

    Change bot name from MALU XD to AGNI

--- plugins/menu.js
+++ plugins/menu.js
@@ -42,10 +42,10 @@ cmd(
       }
 
       const madeMenu = `𝐘𝐨𝐨  ${user}
-*Wᴇʟᴄᴏᴍᴇ Tᴏ 𝗠𝗔𝗟𝗨 𝗫𝗗* 
+*Wᴇʟᴄᴏᴍᴇ Tᴏ AGNI* 
 
 ╭─「 🛠️ ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ 」 
-│🤖 *Bot*: 𝗠𝗔𝗟𝗨 𝗫𝗗
+│🤖 *Bot*: AGNI
 │🙋‍♂️ *User*: ${user}
 │📱 *Owner*: ${owner}
 │⏳ *Uptime*: ${uptime}
@@ -77,14 +77,14 @@ cmd(
 │   ➥ .tts <text> 
 ╰──────────●●►
 
-> *POWERED BY 𝗠𝗔𝗟𝗨 𝗫𝗗*
+> *POWERED BY AGNI*
 `;
 
       await malvin.sendMessage(
         from,
         {
           image: {
-            url: "https://i.ibb.co/SDWZFh23/malvin-xd.jpg",
+            url: "https://files.catbox.moe/8bkx4q.jpg",
           },
           caption: madeMenu,
         },
